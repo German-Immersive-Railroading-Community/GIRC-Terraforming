3cd8481affcf1867861452c6a0a8f021531304cf
MrTroble <nifroh56@gmail.com>
2021-07-22 21:34:29 +0200

    Inital commit

---
+++ src/main/java/eu/girc/terra/TerraformMain.java
@@ -0,0 +1,22 @@
+package eu.girc.terra;
+
+import org.apache.logging.log4j.Logger;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = TerraformMain.MODID)
+public class TerraformMain
+{
+    public static final String MODID = "gircterraform";
+
+    public static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+}
